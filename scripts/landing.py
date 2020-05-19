7813ed6458ef49c87c993607b34a4c2e2410db48
atharvasaraf <atharvasaraf@yahoo.in>
2020-05-20 02:51:05 +0530

    Landing Module Update

--- scripts/landing.py
+++ scripts/landing.py
@@ -33,7 +33,7 @@ from math import copysign, log10
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import PoseStamped
 
-# 60, 12
+
 class LandingModule:
 	def __init__(self):
 		"""
@@ -44,10 +44,10 @@ class LandingModule:
 
 		Outline general flow of script
 		"""
-		# rospy.init_node('landing_node')
-		# self.image_subscriber = rospy.Subscriber("/iris/tiltCam/image_raw/imagestream", Image, self.img_callback)
+		rospy.init_node('landing_node')
+		self.image_subscriber = rospy.Subscriber("/iris/tiltCam/image_raw/imagestream", Image, self.img_callback)
+		
 		self.diameter = 4.00
-
 		self.HSV_min = np.array([0, 128, 95])
 		self.HSV_max = np.array([179, 255, 136])
 
@@ -55,19 +55,26 @@ class LandingModule:
 		# f = (img_width / 2) / ( tan(deg2rad(h_fieldOfView) / 2) )
 		self.img_width = 640
 		self.img_height = 400
-		self.centreImageX = self.img_width / 2
-		self.centreImageY = self.img_height / 2
+		self.centreImageX = self.img_width * 0.5
+		self.centreImageY = self.img_height * 0.5
 		h_fov = 1.0472 #(In radians use np.deg2rad() to convert)
-		self.focal_length = (self.img_width / 2) / ( math.tan( h_fov / 2 ) ) 
-		
+		self.focal_length = (self.img_width * 0.5) / ( math.tan( h_fov * 0.5) ) 
+		self.camera_tilt = 0.4 #Enter the downward tilt of the camera in radians
+
 		self.bufferLength = 4
 		self.HuMomentsFromImageBuffer = None
 		self.HuMomentsFromEquationBuffer = None
+		self.processImage()
 
+	def processImage(self):
+		"""
+		All Image Processing and Data Collection and Matching is done here.
+		"""
 		self.sourceImage()
 
 		self.getHSVmask()
 		self.getExtremePoints()
+
 		self.getCentrePoint()
 		self.getDistanceExtremePoints()
 		self.getDiameterImagePlane()
@@ -75,12 +82,30 @@ class LandingModule:
 		
 		self.getDistanceLandingMarkerFromFocus()
 		self.getHeightDistanceFromFocus()
-		
+		self.correctForCameraOrientation()
 		self.loadSurfaceCoeff()
 		self.getMomentFromEquation()
 		self.getMomentFromImage()
-		# self.loadHuMomentsToBuffer()
-		# self.showExtremePoints()
+		self.loadHuMomentsToBuffer()
+		self.getMeanAndStandardDeviation()
+		self.confirmDome()
+		self.showExtremePoints()
+
+	def sourceImage(self, topic='default'):
+		"""
+		Function to define source of image for Hu Moment extraction	
+		"""
+		if topic == 'default':
+			self.image = cv2.imread('/home/fatguru/catkin_ws/src/btp/raw_data/image/20200427-164100/20200427-165151.jpg')
+		elif topic == 'ROS':
+			self.image = self.sub_image
+
+	def getHSVmask(self):
+		"""
+		Generate Binary Image from HSV thresholding
+		"""
+		self.hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+		self.mask = cv2.inRange(self.hsv, self.HSV_min, self.HSV_max)
 
 	def getExtremePoints(self):
 		"""
@@ -98,6 +123,8 @@ class LandingModule:
 
  		self.extMin = tuple(c[c[:, :, 0].argmin()][0])	
  		self.extMax = tuple(c[c[:, :, 0].argmax()][0])
+		print "Min Extreme Point:", self.extMin
+		print "Max Extreme Point:", self.extMax
 
 	def showExtremePoints(self):	
  		"""
@@ -107,27 +134,17 @@ class LandingModule:
  		cv2.circle(self.image, self.extMax, 8, (0, 255, 0), -1)
 		self.showImage()
 
-	def getDiameterImagePlane(self):
-		"""
-		Simple trignometric calculation to obtain distance between extremum points(Image)
-		"""
-		self.diameterImagePlane = math.sqrt((self.extMax[0] - self.extMin[0])**2 + (self.extMax[1] - self.extMin[1])**2)
-
 	def getCentrePoint(self):
 		# From Top Left Corner
-		self.centreX = (self.extMax[0] + self.extMin[0]) / 2
-		self.centreY = (self.extMax[1] + self.extMin[1]) / 2
+		self.centreX = float(self.extMax[0] + self.extMin[0])* 0.5
+		self.centreY = float(self.extMax[1] + self.extMin[1])* 0.5
 		# From centre
-		self.centreX = self.centreX - (self.img_width / 2)
-		self.centreY = self.centreY - (self.img_height / 2)
+		self.centreX = self.centreX - (self.img_width * 0.5)
+		self.centreY = self.centreY - (self.img_height * 0.5)
 
-	def getHSVmask(self):
-		"""
-		Generate Binary Image from HSV thresholding
-		"""
-		self.hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-		self.mask = cv2.inRange(self.hsv, self.HSV_min, self.HSV_max)
-	
+		print "X Centre:", self.centreX
+		print "Y Centre:", self.centreY
+
 	def getDistanceExtremePoints(self):
 		"""
 		Calculate the distance of the extremum points from the centre of the Image (oP1 and oP2)
@@ -140,20 +157,36 @@ class LandingModule:
 		# Distance of extremum points from focus (fP1 anf fP2)
 		self.extMaxfromFocus = math.sqrt( self.extMaxFromCentre**2 + self.focal_length**2 )
 		self.extMinfromFocus = math.sqrt( self.extMinFromCentre**2 + self.focal_length**2 )
+		
+		print "Max From Centre", self.extMaxFromCentre
+		print "Min From Centre", self.extMinFromCentre
+		
+		print "Max From Focus", self.extMaxfromFocus
+		print "Min From Focus", self.extMinfromFocus
 
+	def getDiameterImagePlane(self):
+		"""
+		Simple trignometric calculation to obtain distance between extremum points(Image)
+		"""
+		self.diameterImagePlane = math.sqrt((self.extMax[0] - self.extMin[0])**2 + (self.extMax[1] - self.extMin[1])**2)
+		print "Diameter Image Plane", self.diameterImagePlane
+	
 	def getAngleFromFocus(self):
 		"""
 		Calculate the angle between the lines passing through focus intersecting the two extreme points on the diameter
 		Using theta = arccos(b**2 + c**2 - a**2 / 2*b*c)
+		Angle is obtained in radians
 		"""
 		self.theta = np.arccos( (self.extMaxfromFocus**2 + self.extMinfromFocus**2 - self.diameterImagePlane**2) / (2* self.extMinfromFocus* self.extMaxfromFocus) )
+		print "Angle From Focus", self.theta
 
 	def getDistanceLandingMarkerFromFocus(self):
 		"""
 		Get the distance of the Landing Marker from Focus
 		Equation (17) in Paper 
 		"""
-		self.zDome = self.diameter / (2* np.tan(self.theta/ 2))
+		self.zDome = (self.diameter * 0.5) / (np.tan(self.theta* 0.5))
+		print "Distance of Centre from Focus:", self.zDome 
 
 	def getHeightDistanceFromFocus(self):
 		"""
@@ -164,51 +197,58 @@ class LandingModule:
 		self.heightFromMarker = self.zDome* np.sin(self.alpha)
 		self.distanceFromMarker = self.zDome* np.cos(self.alpha)
 
-	def showImage(self):
+		print "Angle alpha: ", self.alpha
+		print "Height From Marker: ", self.heightFromMarker
+		print "Distance From Marker: ", self.distanceFromMarker
+	
+	def correctForCameraOrientation(self):
 		"""
-		Show image obtained on topic
+		Assuming that our principal axis does not run parallel to the ground but at a given angle to it.
+		This angle is available from the Gazebo File where the camera position is set
+		The height and depth measured from the camera, need to be adjusted to obtain the actual height and depth of the landing marker from 
+		the drone. A simple rotation matrix can be used to adjust the frame of reference
 		"""
-		cv2.imshow('image', self.image)
-		cv2.waitKey(0)
+		cx = math.cos(self.camera_tilt)
+		sx = math.sin(self.camera_tilt)
+		R = np.array([[cx, -sx],[sx, cx]])
+		vec = np.dot(R, np.array([[self.distanceFromMarker], [self.heightFromMarker]]))
+		self.distanceFromMarker = vec[0]
+		self.heightFromMarker = vec[1]
 
-	def sourceImage(self, topic='default'):
-		"""
-		Function to define source of image for Hu Moment extraction	
-		"""
-		if topic == 'default':
-			self.image = cv2.imread('/home/fatguru/catkin_ws/src/btp/raw_data/image/20200427-164100/20200427-165008.jpg')
-		elif topic == 'ROS':
-			self.image = self.sub_image
-
-	def img_callback(self, ros_data):
-		"""
-		Callback for image_Subcriber topic
-		Saves topic image at self.sub_image
-		"""
-		np_arr = np.fromstring(ros_data.data, np.uint8).reshape(ros_data.height, ros_data.width, 3)
-		raw = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-		self.sub_image = np_arr
+		print "Adjusted Height: ", self.distanceFromMarker
+		print "Adjusted Distance", self.heightFromMarker
 
 	def loadSurfaceCoeff(self):
 		"""
 		Load coefficients of least squares surface obtained
 		"""
-		self.surface_coeff = np.loadtxt('/home/fatguru/catkin_ws/src/btp/learned_moments/20200515-145019', delimiter=',')
+		self.surface_coeff = np.loadtxt('/home/fatguru/catkin_ws/src/btp/learned_moments/20200519-171152', delimiter=',')
+
+		print ""
+		print "Surface coefficients"
+		print self.surface_coeff
+		print ""
 
 	def getMomentFromEquation(self):
 		"""
 		Load HuMoment from Equation
 		"""
+		# x0 = self.distanceFromMarker
+		# Compensating for change in frame of reference
 		x0 = self.distanceFromMarker
 		x1 = self.heightFromMarker
+		print "x0", x0
+		print "x1", x1
 		x0_square = x0**2
 		x1_square = x1**2
 		x0_x1 = x0*x1
 		vec = np.array([[x0, x1, x0_square, x1_square, x0_x1, 1]])
 		self.HuMomentsFromEquation = np.dot(vec, self.surface_coeff).T
-		
-		# print "Equation", self.HuMomentsFromEquation.shape
-		# print self.HuMomentsFromEquation
+		self.HuMomentsFromEquation = np.array([self.HuMomentsFromEquation[0][0], self.HuMomentsFromEquation[1][0], self.HuMomentsFromEquation[2][0], self.HuMomentsFromEquation[3][0]]).reshape(4, 1)
+		print ""
+		print "Moment from Equation:", self.HuMomentsFromEquation.shape
+		print self.HuMomentsFromEquation
+		print ""
 
 	def getMomentFromImage(self):
 		"""
@@ -220,16 +260,18 @@ class LandingModule:
 		# Pick only the first 4 Moments
 		self.HuMomentsFromImage = self.HuMomentsFromImage[0:4]
 		
-		# print "Image", self.HuMomentsFromImage.shape
-		# print self.HuMomentsFromImage	
-
+		print ""
+		print "Image", self.HuMomentsFromImage.shape
+		print self.HuMomentsFromImage	
+		print ""
+	
 	def loadHuMomentsToBuffer(self):
 		if self.HuMomentsFromEquationBuffer == None:
 			self.HuMomentsFromEquationBuffer = self.HuMomentsFromEquation
 		else:
 			self.HuMomentsFromEquationBuffer = np.append(self.HuMomentsFromEquation, self.HuMomentsFromEquationBuffer, axis = 1)
 		
-		if self.HuMomentsFromEquationBuffer.shape[1] > self.bufferLength
+		if self.HuMomentsFromEquationBuffer.shape[1] > self.bufferLength:
 			self.HuMomentsFromEquationBuffer = np.delete(self.HuMomentsFromEquationBuffer, self.bufferLength - 1, 1)
 
 		if self.HuMomentsFromImageBuffer == None:
@@ -237,14 +279,23 @@ class LandingModule:
 		else:
 			self.HuMomentsFromImageBuffer = np.append(self.HuMomentsFromImage, self.HuMomentsFromImageBuffer, axis = 1)
 		
-		if self.HuMomentsFromImageBuffer.shape[1] > self.bufferLength
+		if self.HuMomentsFromImageBuffer.shape[1] > self.bufferLength:
 			self.HuMomentsFromImageBuffer = np.delete(self.HuMomentsFromImageBuffer, self.bufferLength - 1, 1)
+
+		print ""
+		print "HuMomentBufferImage", self.HuMomentsFromImageBuffer.shape
+		print self.HuMomentsFromImageBuffer	
+		print ""
+
+		print ""
+		print "HuMomentBufferEquation", self.HuMomentsFromEquationBuffer.shape
+		print self.HuMomentsFromEquationBuffer	
+		print ""
 
 	def getMeanAndStandardDeviation(self):
 		
 		self.HuImageMean = np.mean(self.HuMomentsFromImageBuffer, axis=1)
 		self.HuImageStdDeviation = np.std(self.HuMomentsFromImageBuffer, axis=1)
-
 		self.HuEquationMean = np.mean(self.HuMomentsFromEquationBuffer, axis=1)
 		self.HuEquationStdDeviation = np.std(self.HuMomentsFromEquationBuffer, axis=1)
 
@@ -252,14 +303,30 @@ class LandingModule:
 
 		# lowerLimit = (0.9* HuEquationMean) 
 		# upperLimit = (1.1* HuEquationMean) 
-		lowerLimit = (0.9* HuEquationMean) - (self.HuImageStdDeviation / 2)
-		upperLimit = (1.1* HuEquationMean) + (self.HuImageStdDeviation / 2)
+		lowerLimit = (0.9* self.HuEquationMean) - (self.HuImageStdDeviation / 2)
+		upperLimit = (1.1* self.HuEquationMean) + (self.HuImageStdDeviation / 2)
 
 		if ((self.HuImageMean >= lowerLimit).all() and (self.HuImageMean <= upperLimit).all()):
 			self.confirmation = True
 			print "Dome Confirmed, Landing Command can be issued."
 		else:
 			self.confirmation = False
+
+	def img_callback(self, ros_data):
+		"""
+		Callback for image_Subcriber topic
+		Saves topic image at self.sub_image
+		"""
+		np_arr = np.fromstring(ros_data.data, np.uint8).reshape(ros_data.height, ros_data.width, 3)
+		raw = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+		self.sub_image = np_arr
+
+	def showImage(self):
+		"""
+		Show image obtained on topic
+		"""
+		cv2.imshow('image', self.image)
+		cv2.waitKey(0)
 
 def main():
 	"""
--- scripts/least_square_fit.py
+++ scripts/least_square_fit.py
@@ -5,6 +5,7 @@ The nomenclature pattern for the class objects, functions, and variables may not
 Least square fit for hu-moment data!
 """
 import time
+import math
 import numpy as np 
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
@@ -13,10 +14,13 @@ from scipy.optimize import least_squares
 class fitData:
 	def __init__(self):
 		self.raw_data = []
+		self.initial_distance_from_drone = 100
 		self.loadData()
+		self.correctOrientation()
 		self.augmentData(self.x_raw, self.z_raw)
 		self.theta = self.simplefit(self.augX, self.augY)
 		self.genPlot(self.theta)
+		# self.makePlot()
 		self.requestSaveData()
 		# self.testfit()
 
@@ -33,6 +37,17 @@ class fitData:
 		self.genPlot(self.theta)
 		print th
 
+	def correctOrientation(self):
+		self.x_raw = self.initial_distance_from_drone - self.x_raw
+		# self.camera_tilt = 0.4
+		# cx = math.cos(self.camera_tilt)
+		# sx = math.sin(self.camera_tilt)
+		# R = np.array([[cx, sx],[-sx, cx]])
+		# stack = np.append(self.x_raw.T, self.z_raw.T, axis=0)
+		# vec = np.dot(R, stack)
+		# self.x_raw = vec[0][:].reshape(self.x_raw.shape[0], self.x_raw.shape[1])
+		# self.z_raw = vec[1][:].reshape(self.z_raw.shape[0], self.z_raw.shape[1])
+
 	def augmentData(self, x0, x1):
 		self.x0 = x0
 		self.x1 = x1
@@ -41,8 +56,8 @@ class fitData:
 		x0_x1 = self.x0 * self.x1
 		# Normalize matrices if required
 		self.augX = np.hstack((self.x0, self.x1, x0_square, x1_square, x0_x1, np.ones((np.shape(self.x0)[0],1))))
-		self.normalizehu()
 		self.augY = np.hstack((self.hu1, self.hu2, self.hu3, self.hu4))
+		# self.normalizehu()
 	
 	def normalizehu(self):
 		i = self.hu1
@@ -67,7 +82,7 @@ class fitData:
 
 	def loadData(self):
 		self.raw_data = np.loadtxt('/home/fatguru/catkin_ws/src/btp/data/20200427-170305', delimiter=',')
-		self.raw_data = self.raw_data[0:31][:] # Latest data generated with self.raw_data[0:31][:]
+		self.raw_data = self.raw_data[:][:] # Latest data generated with self.raw_data[0:31][:]
 		self.x_raw, self.y_raw, self.z_raw, self.hu1, self.hu2, self.hu3, self.hu4, self.hu5, self.hu6, self.hu7 = np.hsplit(self.raw_data, np.array([1,2,3,4,5,6,7,8,9]))
 	
 		#Add reference frame offset here
@@ -78,38 +93,94 @@ class fitData:
 		theta = np.dot(np.dot(np.linalg.inv(np.dot(Xdata.T, Xdata)), Xdata.T), Ydata)
 		return theta
 
+	def f0(self, x, y):
+		p = (self.theta[0][0]*x + self.theta[1][0]*y + self.theta[2][0]*(x**2) + self.theta[3][0]*(y**2) + self.theta[4][0]*(x*y) + self.theta[5][0])
+		# print p.shape
+		return p
+
+	def f1(self, x, y):
+		p = (self.theta[0][1]*x + self.theta[1][1]*y + self.theta[2][1]*(x**2) + self.theta[3][1]*(y**2) + self.theta[4][1]*(x*y) + self.theta[5][1])
+		# print p.shape
+		return p
+
+	def f2(self, x, y):
+		p = (self.theta[0][2]*x + self.theta[1][2]*y + self.theta[2][2]*(x**2) + self.theta[3][2]*(y**2) + self.theta[4][2]*(x*y) + self.theta[5][2])
+		# print p.shape
+		return p
+
+	def f3(self, x, y):
+		p = (self.theta[0][3]*x + self.theta[1][3]*y + self.theta[2][3]*(x**2) + self.theta[3][3]*(y**2) + self.theta[4][3]*(x*y) + self.theta[5][3])
+		# print p.shape
+		return p
+
+	def makePlot(self):
+		step_size = 30
+		x_temp = np.linspace(np.min(self.x0), np.max(self.x0), step_size).reshape(step_size, 1)
+		y_temp = np.linspace(np.min(self.x1), np.max(self.x1), step_size).reshape(step_size, 1)
+		X, Y = np.meshgrid(x_temp, y_temp)
+		z_temp = self.f0(X, Y)
+		print X.shape
+		fig = plt.figure()
+		ax = plt.axes(projection='3d')
+		ax.plot_surface(X, Y, z_temp, cmap='viridis', edgecolor='none')
+		# ax.contour3D(X, Y, z_temp, 50, cmap='binary')
+		plt.show()
+
 	def genPlot(self, theta):
 		step_size = 20
 		x_temp = np.linspace(np.min(self.x0), np.max(self.x0), step_size).reshape(step_size, 1)
 		y_temp = np.linspace(np.min(self.x1), np.max(self.x1), step_size).reshape(step_size, 1)
-		foo = np.hstack((x_temp, y_temp, x_temp**2, y_temp**2, x_temp*y_temp, np.ones((step_size,1))))
-		outval = np.dot(foo, theta)
-		x = np.outer(x_temp, np.ones(step_size))
-		y = np.outer(x_temp, np.ones(step_size)).T
+		# foo = np.hstack((x_temp, y_temp, x_temp**2, y_temp**2, x_temp*y_temp, np.ones((step_size,1))))
+		# outval = np.dot(foo, theta)
+		# x = np.outer(x_temp, np.ones(step_size))
+		# y = np.outer(x_temp, np.ones(step_size)).T
 		# y = np.outer(y_temp, np.ones(step_size)).T
+		X, Y = np.meshgrid(x_temp, y_temp)
+		z0_temp = self.f0(X, Y)
+		z1_temp = self.f1(X, Y)
+		z2_temp = self.f2(X, Y)
+		z3_temp = self.f3(X, Y)
 		
 		fig = plt.figure()
 
+		# ax = fig.add_subplot(2,2,1,projection = '3d')
+		# ax.plot_surface(x, y, outval[:, 0].reshape(step_size,1), cmap='viridis', edgecolor='none')
+		# ax.scatter(self.x0, self.x1, self.augY[:, 0], marker='^', color='r')
+		# ax.set_title('Hu1')
+
+		# ax = fig.add_subplot(2,2,2,projection = '3d')
+		# ax.plot_surface(x, y, outval[:, 1].reshape(step_size,1), cmap='viridis', edgecolor='none')
+		# ax.scatter(self.x0, self.x1, self.augY[:, 1], marker='^', color='r')
+		# ax.set_title('Hu2')
+
+		# ax = fig.add_subplot(2,2,3,projection = '3d')
+		# ax.plot_surface(x, y, outval[:, 2].reshape(step_size,1), cmap='viridis', edgecolor='none')
+		# ax.scatter(self.x0, self.x1, self.augY[:, 2], marker='^', color='r')
+		# ax.set_title('Hu3')
+
+		# ax = fig.add_subplot(2,2,4,projection = '3d')
+		# ax.plot_surface(x, y, outval[:, 3].reshape(step_size,1), cmap='viridis', edgecolor='none')
+		# ax.scatter(self.x0, self.x1, self.augY[:, 3], marker='^', color='r')
+		# ax.set_title('Hu4')
 		ax = fig.add_subplot(2,2,1,projection = '3d')
-		ax.plot_surface(x, y, outval[:, 0].reshape(step_size,1), cmap='viridis', edgecolor='none')
+		ax.plot_surface(X, Y, z0_temp, cmap='viridis', edgecolor='none')
 		ax.scatter(self.x0, self.x1, self.augY[:, 0], marker='^', color='r')
 		ax.set_title('Hu1')
 
 		ax = fig.add_subplot(2,2,2,projection = '3d')
-		ax.plot_surface(x, y, outval[:, 1].reshape(step_size,1), cmap='viridis', edgecolor='none')
+		ax.plot_surface(X, Y, z1_temp, cmap='viridis', edgecolor='none')
 		ax.scatter(self.x0, self.x1, self.augY[:, 1], marker='^', color='r')
 		ax.set_title('Hu2')
 
 		ax = fig.add_subplot(2,2,3,projection = '3d')
-		ax.plot_surface(x, y, outval[:, 2].reshape(step_size,1), cmap='viridis', edgecolor='none')
+		ax.plot_surface(X, Y, z2_temp, cmap='viridis', edgecolor='none')
 		ax.scatter(self.x0, self.x1, self.augY[:, 2], marker='^', color='r')
 		ax.set_title('Hu3')
 
 		ax = fig.add_subplot(2,2,4,projection = '3d')
-		ax.plot_surface(x, y, outval[:, 3].reshape(step_size,1), cmap='viridis', edgecolor='none')
+		ax.plot_surface(X, Y, z3_temp, cmap='viridis', edgecolor='none')
 		ax.scatter(self.x0, self.x1, self.augY[:, 3], marker='^', color='r')
 		ax.set_title('Hu4')
-		
 		plt.show()
 
 	def requestSaveData(self):
@@ -118,6 +189,7 @@ class fitData:
 			self.saveData()
 		else:
 			print "Exiting"
+	
 	def saveData(self):
 		"""
 		Method to save obtained Least-Square Fit Surface to below path
@@ -134,3 +206,24 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+
+
+
+
+	"""
+	REFERENCE CODES:
+
+		def testfit(self):
+		rnd = np.random.RandomState(1)
+		x0 = np.linspace(1, 10, 10).reshape((10,1))
+		x1 = np.linspace(51, 60, 10).reshape((10,1))
+		error = rnd.randn(10).reshape((10,1))
+		x0 = x0 + 0.25*error
+		x1 = x1 + 0.35*error
+		self.augmentData(x0, x1)
+		self.augY = np.linspace(52, 70, 10).reshape((10,1))
+		self.theta = self.simplefit(self.augX, self.augY)
+		self.genPlot(self.theta)
+		print th
+	"""
